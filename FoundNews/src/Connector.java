b3fee839f31d38bbb9bf24c268dc0c0ba328097f
vivian2tina <vivan_0920@yahoo.com.tw>
2019-12-06 11:54:02 +0800

    //20191206

---
+++ FoundNews/src/Connector.java
@@ -0,0 +1,22 @@
+
+public class Connector {
+	public String url;
+	public Node a;//have to modify
+	public Connector(String url) {
+		this.url=url;
+	}
+	
+	public String sentToBackSide() {
+		return url;
+	}
+	
+	public Node acceptResult() {
+		return a;
+	}
+	
+	
+	
+	
+	
+	
+}
